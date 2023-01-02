ca483edb37a10224f7528cfa968681a40672cb4b
zzzmm21 <ghdfhr4353@gmail.com>
2023-01-02 17:20:39 +0900

    django

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('create/',views.create_bulletin, name='new_bulletin'),
     path('add/',views.add_bulletin, name='insert_bulletin'),
     path('<int:bulletin_id>/', views.view_bulletin, name='view'),
+    path('update/<int:bulletin_id>/', views.update_bulletin, name='update'),
+    path('delete/<int:bulletin_id>/', views.delete_bulletin, name='delete'),
 
 ]
--- bulletin_board/views.py
+++ bulletin_board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Bulletin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
@@ -25,3 +26,20 @@ def add_bulletin(request):
 def view_bulletin(request, bulletin_id):
     bulletin = get_object_or_404(Bulletin, pk=bulletin_id)
     return render(request, 'bulletin/detail.html', {'bulletin':bulletin})
+
+def update_bulletin(request, bulletin_id):
+    bulletin = Bulletin.objects.get(id=bulletin_id)
+
+    if request.method == 'POST':
+        bulletin.title = request.POST['title']
+        bulletin.content = request.POST['content']
+        bulletin.writeDate = timezone.datetime.now()
+        bulletin.save()
+        return HttpResponseRedirect(reverse('bulletin_board:view', args=(bulletin_id,)))
+    else:
+        return render(request, 'bulletin/detail.html', {'bulletin': bulletin})
+
+def delete_bulletin(request, bulletin_id):
+    bulletin = Bulletin.objects.get(id=bulletin_id)
+    bulletin.delete()
+    return HttpResponseRedirect(reverse('bulletin_board:index'))
