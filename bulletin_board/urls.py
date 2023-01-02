eab7c2e697f3ee003919ae20dc81c3c70db3abc7
zzzmm21 <ghdfhr4353@gmail.com>
2023-01-02 14:43:52 +0900

    django

--- bulletin_board/urls.py
+++ bulletin_board/urls.py
@@ -4,5 +4,7 @@ from . import  views
 app_name = 'bulletin_board'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create',views.create_bulletin, name='new_bulletin'),
+    path('add',views.add_bulletin, name='insert_bulletin'),
 
 ]
--- bulletin_board/views.py
+++ bulletin_board/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from .models import Bulletin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    bulletin_list = Bulletin.objects.all().oredr_by('-writeDate')
+    bulletin_list = Bulletin.objects.all().order_by('-writeDate')
+    context = {'bulletin_list': bulletin_list}
+    return render(request, 'bulletin/index.html', context)
+
+def create_bulletin(request):
+    return render(request, 'bulletin/create_bulletin.html')
+
+def add_bulletin(request):
+    bulletin = Bulletin()
+    bulletin.title = request.POST['title']
+    bulletin.content = request.POST['content']
+    bulletin.name = request.POST['name']
+    bulletin.passwd = request.POST['pincode']
+    bulletin.save()
+
+    return HttpResponseRedirect(reverse('bulletin_board:index'))
+
